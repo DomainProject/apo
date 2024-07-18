f699a3c00eff0cefb77cf40cf997be4f6eade551
Emanuele De Angelis <emanuele.de.angelis30@gmail.com>
2024-07-18 16:51:25 +0200

    updated interface

--- C/ddm.c
+++ C/ddm.c
@@ -6,13 +6,15 @@
 
 static int num_cus;
 static enum cu_type *mapping_cus;
+static int *cu_capacity; 
 
 void ddm_init(
     int total_cus,
     int total_actors,
     enum cu_type *cus,
     int msg_exch_cost[total_cus][total_cus],
-    short runnable_on [total_actors])
+    short runnable_on[total_actors],
+    int cu_capacity[total_cus])
 {
     num_cus = total_cus;
     mapping_cus = cus;
@@ -21,40 +23,40 @@ void ddm_init(
     printf("actor(1..%d).\n", total_actors);
 
     // runnable_on/2
-    // *** TODO: combinazioni?
-    for(int i=0; i<total_actors; ++i)
-      switch(runnable_on[i]) {
-        case 1: 
-          printf("runnable_on(%d,cpu)",i);
-          break;
-        case 2: 
-          printf("runnable_on(%d,gpu)",i);
-          break;
-        case 4: 
-          printf("runnable_on(%d,fpga)",i);
-          break;
-        // combinazioni
-        default: 
-         printf("???"); break;
-      }
-
+    for(int i=0; i<total_actors; ++i) {
+      if (runnable_on[i] & 1)
+        printf("runnable_on(%d,cpu)",i);
+      if (runnable_on[i] & (1 << 2))
+        printf("runnable_on(%d,gpu)",i);
+      if (runnable_on[i] & (1 << 3))
+        printf("runnable_on(%d,fpga)",i);
+    }
    
     // cu/1
     printf("cu(1..%d).\n", total_cus);
 
-    // cu_type/2
-    // *** TODO: non rappresentato
-
-    // cu_capacity/2 
-    // *** TODO: non rappresentato
+    
+    for(int i=0; i<total_cus; ++i) {
+      // cu_type/2
+      switch(cus[i]) {
+        case CPU:
+          printf("cu_type(%d,cpu)",i);
+          break;
+        case GPU:
+          printf("cu_type(%d,gpu)",i);
+          break;
+        case FPGA:  
+          printf("cu_type(%d,fpga)",i);
+          break;
+      }
+      // cu_capacity/2 
+      printf("cu_capacity(%d)",cu_capacity[i]);
+    }
 
     // msg_exch_cost/3
-    // TODO: vedi definifizione in dd_v4_init.asp
-    // msg_exch_cost(cpu,cpu,1). % c'è il tipo, dobbiamo usare l'identificatiore della specifica CU?
-    // cambiamo la definizione di a_cc(A1,A2,C)? invece del tipo usiamo l'identificatore della CU
     for(int i=0; i<total_cus; ++i)
       for(int j=0; i<total_cus; ++j)
-      printf("msg_exch_cost(%d,%d,%d)",i,j,msg_exch_cost[i][j]);
+        printf("msg_exch_cost(%d,%d,%d)",i,j,msg_exch_cost[i][j]);
 
 }
 
@@ -65,12 +67,27 @@ enum cu_type *ddm_optimize(
     int tasks_forecast[total_actors])
 {
     // tasks_forecast/2
+    for(int i=0; i<total_actors; ++i) {
+      printf("tasks_forecast(%d,%d)",i,tasks_forecast[i]);
+    }    
 
-    // mutual_annoyance/3
+    for(int i=0; i<total_actors; ++i) {
+      for(int j=0; j<total_actors; ++j) {
+        // msg_exchange_rate/3
+        if(actors[i][j].msg_exchange_rate)
+          printf("msg_exchange_rate(%d,%d,%d)",i,j,
+            actors[i][j].msg_exchange_rate);
+        // mutual_annoyance/3
+        if(actors[i][j].annoyance)
+          printf("mutual_annoyance(%d,%d,%d)",i,j,
+            actors[i][j].annoyance);
+      }
+    } 
 
     // msg_exch_rate/3
 
-    // see clingo_model_to_strings
+    // TODO: add get_model to libasp_solver
+    // that returns a clingo model (not an array of strings)
     char *atom, *snd, *end;
     for(int i=0; i<total_actors; ++i) {
       atom +=7; // skip run_on(
--- C/ddm.h
+++ C/ddm.h
@@ -7,5 +7,14 @@ struct actor_matrix {
     int msg_exchange_rate;
 };
 
-extern void ddm_init(int total_cus, int total_actors, enum cu_type *cus, int msg_exch_cost[total_cus][total_cus], short runnable_on[total_actors]);
-extern enum cu_type *ddm_optimize(int total_actors, struct actor_matrix actors[total_actors][total_actors], int tasks_forecast[total_actors]);
+extern void ddm_init(
+    int total_cus, 
+    int total_actors, 
+    enum cu_type *cus, 
+    int msg_exch_cost[total_cus][total_cus], 
+    short runnable_on[total_actors],
+    int cu_capacity[total_cus]);
+extern enum cu_type *ddm_optimize(
+    int total_actors, 
+    struct actor_matrix actors[total_actors][total_actors], 
+    int tasks_forecast[total_actors]);
