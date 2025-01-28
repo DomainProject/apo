12a6db1497b36a5c0e6a3432215d4fe5a6a309a2
Romolo Marotta <romolo.marotta@gmail.com>
2025-01-28 18:08:38 +0100

    Refactored hardware parameters

---
+++ metasimulation/SimulationParameters/global_constants.py
@@ -0,0 +1,5 @@
+skip_queue_validation = True
+ending_simulation = False
+time_window_size = 500  # milliseconds
+task_unit_costs = 0.040  # milliseconds to execute an event
+task_anno_costs = 0.100  # milliseconds to undo an event
