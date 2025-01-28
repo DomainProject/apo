481efb60a0330488a5170dbaa1290697758d8f8d
Romolo Marotta <romolo.marotta@gmail.com>
2025-01-28 17:59:55 +0100

    added new debug traces

--- metasimulation/generate_debug_traces.sh
+++ metasimulation/generate_debug_traces.sh
@@ -2,5 +2,9 @@
 
 mkdir -p debug_traces
 python3 trace_generator.py 10 50000 -1
+python3 trace_generator.py 10 50000 10
 python3 trace_generator.py 10 50000 100
+python3 trace_generator.py 10 50000 500
+python3 trace_generator.py 10 100 -1
+python3 trace_generator.py 10 100 10
 mv *.trace debug_traces
