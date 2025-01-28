f0d14d51064f9b1ccb03eb8fe9a2c872d5c5e235
Romolo Marotta <romolo.marotta@gmail.com>
2025-01-28 18:24:24 +0100

    Added a bit of comments

--- metasimulation/generate_debug_traces.sh
+++ metasimulation/generate_debug_traces.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# trace generator takes 3 parameters:
+
+# 1: number of actors
+# 2: simulated time end
+# 3: period in simulated time for sending a potential straggler message (-1 means infinity, so no stragglers)
+
 mkdir -p debug_traces
 python3 trace_generator.py 10 50000 -1
 python3 trace_generator.py 10 50000 10
