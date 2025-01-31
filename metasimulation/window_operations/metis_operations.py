42a92cdf3a1268baf0b0915a78e2d9488383305f
federicamontes <federica.montesano42@gmail.com>
2025-01-31 11:56:24 +0100

    added msg_exch_cost in window_operations/metis_operations.py

--- metasimulation/window_operations/metis_operations.py
+++ metasimulation/window_operations/metis_operations.py
@@ -2,7 +2,7 @@ import math
 
 from metasimulation.SimulationEngine.sim import get_events_count_vector_in_next_window
 from metasimulation.SimulationModel.hardware import build_cunits, get_capacity_vector, \
-    convert_metis_assignment_to_sim_assingment
+    convert_metis_assignment_to_sim_assingment, get_communication_latency, comm_unitary_cost
 from metasimulation.window_operations.abstract_operations import WindowOperations
 from src.metis import ddmmetis_init, metis_get_partitioning, metis_partitioning
 
@@ -22,9 +22,12 @@ class MetisOperations(WindowOperations):
         min_vt = super().on_window(cu_units_data, wct_ts, ending_simulation, traces, committed_idxs, time_window_size,
                                    communication, annoyance)
         num_actors = self.sim_state.get_num_actors()
-        cus = len(build_cunits())
+        cunits = self.sim_state.get_cunits()
+        cus = len(cunits)
         capacity = get_capacity_vector()
         task_forecast = get_events_count_vector_in_next_window(wct_ts + time_window_size, num_actors)
+        msg_exch_cost = [ [ int(get_communication_latency(x,y)/comm_unitary_cost) for y in cunits] for x in cunits]
+
         comm_matrix = []
         anno_matrix = []
 
@@ -37,7 +40,7 @@ class MetisOperations(WindowOperations):
             comm_matrix.append(comm_row)
             anno_matrix.append(anno_row)
 
-        metis_partitioning(num_actors, cus, task_forecast, capacity, comm_matrix, anno_matrix)
+        metis_partitioning(num_actors, cus, task_forecast, capacity, comm_matrix, anno_matrix, msg_exch_cost)
         return min_vt
 
     def delayed_on_window(self):
