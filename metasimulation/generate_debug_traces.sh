6bc499f18bf77cc3814c077b7af6f8a75b0b1796
Romolo Marotta <romolo.marotta@gmail.com>
2025-01-31 03:23:30 +0100

    Refactored simulation folders
    Now each simulation has its own folder with all the required parameters.
    This should improve reliability of evaluations.
    
    Signed-off-by: Romolo Marotta <romolo.marotta@gmail.com>

--- metasimulation/generate_debug_traces.sh
+++ metasimulation/generate_debug_traces.sh
@@ -6,11 +6,13 @@
 # 2: simulated time end
 # 3: period in simulated time for sending a potential straggler message (-1 means infinity, so no stragglers)
 
-mkdir -p debug_traces
-python3 trace_generator.py 8 50000 -1
-python3 trace_generator.py 8 50000 1
-python3 trace_generator.py 8 50000 10
-python3 trace_generator.py 8 50000 100
-python3 trace_generator.py 8 50000 500
 
-mv *.trace debug_traces
+mkdir -p simulation_1
+python3 trace_generator.py 8 50000 -1
+mv *.trace simulation_1
+
+mkdir -p simulation_2
+python3 trace_generator.py 8 50000 10
+mv *.trace simulation_2
+
+
