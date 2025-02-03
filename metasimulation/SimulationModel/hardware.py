55998a0ddfc36614261a654f39eb0eb5732741d1
federicamontes <federica.montesano42@gmail.com>
2025-02-03 16:17:53 +0100

    added convert_metis_assignment_to_sim_assingment_enhanced to compute an assignment actor→cu considering the speed of the cus

--- metasimulation/SimulationModel/hardware.py
+++ metasimulation/SimulationModel/hardware.py
@@ -62,12 +62,52 @@ def convert_metis_assignment_to_sim_assingment(partition):
         else:
             count_dict[index] = 1
 
+    #print(partition, speed)
     sorted_indices = sorted(count_dict.keys(), key=lambda x: count_dict[x], reverse=True)
 
-    unit_mapping = {index: cunits[i] for i, index in enumerate(sorted_indices)}
 
+    unit_mapping = {index: cunits[i] for i, index in enumerate(sorted_indices)}
     assign = [unit_mapping[i] for i in partition]
 
+
+
+    #print(partition)
+    # round robin assignement
+    # assign = [cunits[i] for i in partition]
+    return assign
+
+def convert_metis_assignment_to_sim_assingment_enhanced(partition, speed):
+    assign = []
+    cunits = build_cunits()
+
+    count_dict = {}
+    for index in partition:
+        if index in count_dict:
+            count_dict[index] += 1
+        else:
+            count_dict[index] = 1
+
+    #print(partition, speed)
+    sorted_indices = sorted(count_dict.keys(), key=lambda x: count_dict[x], reverse=True)
+
+    sorted_cunits = sorted(enumerate(speed), key=lambda x: x[1], reverse=True)
+
+    # Ensure we have enough computing units for the partitions
+    unit_mapping = {}
+    num_cunits = len(sorted_cunits)
+    num_partitions = len(sorted_indices)
+
+    # Limit the mapping to the number of available computing units
+    for i in range(min(num_cunits, num_partitions)):
+        unit_idx = sorted_cunits[i][0]  # The index of the CU in the sorted list
+        unit_mapping[sorted_indices[i]] = cunits[unit_idx]
+
+    #unit_mapping = {index: cunits[i] for i, index in enumerate(sorted_indices)}
+    assign = [unit_mapping[i] for i in partition]
+
+
+
+    #print(partition)
     # round robin assignement
     # assign = [cunits[i] for i in partition]
     return assign
