e0b0b4bbdf8f7b5ffa91620bfb122d99021307ec
Alessandro Pellegrini <a.pellegrini@ing.uniroma2.it>
2025-02-04 15:53:02 +0100

    Switch to more portable resource generation
    Rather than relying on obscure linker options that are
    not entirely portable, there is now a small utility program
    that is used to embed resources in the resource library.
    
    Signed-off-by: Alessandro Pellegrini <a.pellegrini@ing.uniroma2.it>

--- src/lp/assets.h
+++ src/lp/assets.h
@@ -6,29 +6,10 @@
 
 #include <stdlib.h>
 
-#ifdef __APPLE__
-#include <mach-o/getsect.h>
-
-#define EXTLD(NAME) extern const unsigned char _section$__DATA__##NAME[];
-#define LDVAR(NAME) _section$__DATA__##NAME
-#define LDLEN(NAME) (getsectbyname("__DATA", "__" #NAME)->size)
-
-#elif (defined __WIN32__) /* mingw */
-
-#define EXTLD(NAME)                                                                                                    \
-	extern const unsigned char binary_##NAME##_start[];                                                            \
-	extern const unsigned char binary_##NAME##_end[];
-#define LDVAR(NAME) binary_##NAME##_start
-#define LDLEN(NAME) ((binary_##NAME##_end) - (binary_##NAME##_start))
-
-#else /* gnu/linux ld */
-
-#define EXTLD(NAME)                                                                                                    \
-	extern const unsigned char _binary_##NAME##_start[];                                                           \
-	extern const unsigned char _binary_##NAME##_end[];
-#define LDVAR(NAME) (const char *)_binary_##NAME##_start
-#define LDLEN(NAME) ((_binary_##NAME##_end) - (_binary_##NAME##_start))
-#endif
+#define EXTLD(NAME) extern const char NAME[]; \
+                    extern const size_t NAME_len;
+#define LDVAR(NAME) NAME
+#define LDLEN(NAME) NAME_len
 
 // Define resources variables
 EXTLD(ddm_v6_asp)
