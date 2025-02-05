7141377c453d6bc7f7a1917c4803a5507479d2e9
federicamontes <federica.montesano42@gmail.com>
2025-02-05 22:19:45 +0100

    added files: - metis_homogeneous_communication_operations.py - metis_homogeneous_nodes_operations.py

---
+++ metasimulation/window_operations/metis_homogeneous_communication_operations.py
@@ -0,0 +1,57 @@
+import math
+
+from metasimulation.SimulationEngine.runtime_modules import global_constants_parameter_module as global_constants
+from metasimulation.SimulationEngine.runtime_modules import hardware_parameter_module as hardware_constants
+
+from metasimulation.SimulationEngine.sim import get_events_count_vector_in_next_window
+from metasimulation.SimulationModel.hardware import build_cunits, get_capacity_vector, \
+    convert_metis_assignment_to_sim_assingment,  get_communication_latency
+from metasimulation.window_operations.abstract_operations import WindowOperations
+from src.metis import ddmmetis_init, metis_get_partitioning, metis_homogeneous_communication
+
+
+class MetisHomogeneousCommunicationOperations(WindowOperations):
+
+    def __init__(self, sim_state):
+        print(f"initialize Metis...", end='')
+        self.sim_state = sim_state
+        ddmmetis_init(sim_state.get_num_actors())
+        print(f"done")
+
+    def on_window(self, cu_units_data, wct_ts, ending_simulation, traces, committed_idxs, time_window_size,
+                  communication, annoyance):
+        if ending_simulation: return float('inf')
+        min_vt = super().on_window(cu_units_data, wct_ts, ending_simulation, traces, committed_idxs, time_window_size,
+                                   communication, annoyance)
+        num_actors = self.sim_state.get_num_actors()
+        cunits = self.sim_state.get_cunits()
+        cus = len(cunits)
+        capacity = [1] * cus
+        task_forecast = get_events_count_vector_in_next_window(wct_ts + time_window_size, num_actors)
+        comm_matrix = []
+
+        for i in range(num_actors):
+            comm_row = []
+            for j in range(num_actors):
+                comm_row.append(math.ceil(communication[j][i] / wct_ts))
+            comm_matrix.append(comm_row)
+
+        for i in range(len(task_forecast)): task_forecast[i] += 1
+
+        
+       # print("capacity : ", capacity)
+       # print("comm matrix: ", comm_matrix)
+       # print("task forecast: ", task_forecast)
+        metis_homogeneous_communication(num_actors, cus, comm_matrix, task_forecast)
+        return min_vt
+
+    def delayed_on_window(self):
+        # TODO call method to retrieve partitioning and try to install it
+        # if no partitioning has been found return None
+        cunits = self.sim_state.get_cunits()
+        actors = self.sim_state.get_num_actors()
+        speed = []
+        part = metis_get_partitioning()
+        if not part:
+            return None
+        return convert_metis_assignment_to_sim_assingment(part)
