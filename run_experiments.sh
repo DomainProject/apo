0d23c2bf4907a72c1383293677f9538cc4774f4a
Romolo Marotta <romolo.marotta@gmail.com>
2025-03-18 05:03:24 +0100

    Improved script for launching experiments
    Signed-off-by: Romolo Marotta <romolo.marotta@gmail.com>

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-./sim_from_trace.py ddm metasimulation/$1 | tee ddm.log
-#./sim_from_trace.py metis-hete-asplike metasimulation/$1 | tee metis-hete-asplike.log
-#./sim_from_trace.py metis-hete-comm metasimulation/$1 | tee metis-hete-comm.log
-#./sim_from_trace.py metis-homo-comm metasimulation/$1 | tee metis-homo-comm.log
-#./sim_from_trace.py metis-homo-node metasimulation/$1 | tee metis-homo-node.log
-#./sim_from_trace.py random metasimulation/$1 | tee random.log
+simfolder=$1
+models="ddm" #metis-hete-asplike metis-hete-comm metis-homo-comm metis-homo-node random"
 
-mv ddm.log metasimulation/$1
-#mv metis-hete-asplike.log metasimulation/$1
-#mv metis-hete-comm.log metasimulation/$1
-#mv metis-homo-comm.log metasimulation/$1
-#mv metis-homo-node.log metasimulation/$1
-#mv random.log metasimulation/$1
+for m in $models; do
+  echo ./sim_from_trace.py $m $simfolder | tee $simfolder/$m.log
+done
 
-cd metasimulation/$1
-python3 ../../plotta.py ddm.log metis-hete-asplike.log metis-hete-comm.log metis-homo-comm.log metis-homo-node.log random.log
+cd $simfolder
+python3 ../../plotta.py metis-hete-asplike.log metis-hete-comm.log metis-homo-comm.log metis-homo-node.log random.log
 gnuplot plot.plt
